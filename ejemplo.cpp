da9a44a9e55577d2a624ceb375106f94b54342e0
William Oquendo <woquendo@gmail.com>
2021-02-26 12:40:55 -0500

    Mensaje nuevo cambio
    asdasdlkasdlka ajsdlkjsadslkadklsajdsaj

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cstdlib>
+
+// ./a.out -> argc = 1, argv = ["./a.out"]
+// ./a.out 10 -> argc = 2, argv = ["./a.out", "10"]
 
 int main(int argc, char **argv)
 {
-    std::cout << "!Hola mundo!\n";
+    int a = std::atoi(argv[1]);
+    a = 2*a;
 
+    std::cout << "!Hola mundo!\n";
+    std::cout << "2*a: " << a << std::endl;
     return 0;
 }
